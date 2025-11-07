4ed87863f1c21123b94590cbade3f97024ad3b78
namDev <ninhnamhp@gmail.com>
2025-11-07 09:39:18 +0700

    nam done phan my profile

---
+++ src/main/java/com/library/librarymanagement/dto/request/ChangePasswordRequest.java
@@ -0,0 +1,21 @@
+package com.library.librarymanagement.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChangePasswordRequest {
+    @NotBlank(message = "Old password is required")
+    private String oldPassword;
+
+    @NotBlank(message = "New password is required")
+    @Size(min = 8, message = "New password must be at least 8 characters")
+    private String newPassword;
+}
